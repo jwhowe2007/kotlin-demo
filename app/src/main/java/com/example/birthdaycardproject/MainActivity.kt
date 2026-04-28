214ea74d0af68a4d09528d0f83db73126cecb962
Justin Howe <jwhowe02@proton.me>
2026-04-28 09:16:40 -0700

    Add BirthdayCardProject - current state

---
+++ app/src/main/java/com/example/birthdaycardproject/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.example.birthdaycardproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.birthdaycardproject.ui.theme.BirthdayCardProjectTheme
+
+const val personName: String = "Justin"
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BirthdayCardProjectTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(
+                        message = "Happy Birthday, $personName!",
+                        modifier = Modifier
+                            .padding(24.dp)
+                            .background(Color.Green),
+                        from = "Android"
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GreetingText(message: String, modifier: Modifier = Modifier, from: String = "Nobody") {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            modifier = modifier.background(Color.Cyan),
+            fontSize = 80.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "From: $from",
+            modifier = Modifier.padding(16.dp).align(alignment = Alignment.End).background(Color.Cyan),
+            fontSize = 36.sp,
+            textAlign = TextAlign.Right
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    BirthdayCardProjectTheme {
+        val modifier = Modifier
+            .padding(8.dp)
+            .background(Color.Green)
+
+        GreetingText(message = "Happy Birthday, $personName!", modifier = modifier, from = "Android")
+    }
+}
